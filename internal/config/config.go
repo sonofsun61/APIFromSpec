fa888b2c31f5ba4322215832e93d86044532e668
tarnovgm <glebtarn@gmail.com>
2026-09-19 16:10:17 +0300

    feat: add config creation (port + databaseURL)

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,11 +3,21 @@ package config
 import "os"
 
 type Config struct {
-	Port string
+	Port        string
+	DatabaseURL string
 }
 
 func MustLoadConfig() *Config {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		dbURL = "postgres://postgres:123@localhost:8080/shopapi?sslomode=disabled"
+	}
 	return &Config{
-		Port: os.Getenv("PORT"),
+		Port: port,
+		DatabaseURL: dbURL,
 	}
 }
