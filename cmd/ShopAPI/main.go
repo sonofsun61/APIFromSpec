35ec56a2fbcb5ba45ad2a7fd7a221555bc566708
tarnovgm <glebtarn@gmail.com>
2026-09-23 00:50:23 +0300

    feat: add proper connection to pgxpool + check

--- cmd/ShopAPI/main.go
+++ cmd/ShopAPI/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"github.com/sonofsun61/APIFromSpec.git/internal/app"
-	"github.com/sonofsun61/APIFromSpec.git/internal/config"
+	"context"
+	"fmt"
+
+	"github.com/sonofsun61/APIFromSpec/internal/config"
+	"github.com/sonofsun61/APIFromSpec/internal/database"
 )
 
 func main() {
 	cfg := config.MustLoadConfig()
-	application := app.NewApp(cfg)
-	application.Run()
+	ctx := context.Background()
+	pool := database.MustConnectToDatabase(ctx, cfg.ConnString)
+	defer pool.Close()
+	fmt.Println("connected to database successfully")
 }
