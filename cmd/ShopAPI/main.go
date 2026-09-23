53d2c483194fff25100a0af3d017b92982450ac4
tarnovgm <glebtarn@gmail.com>
2026-09-24 00:32:35 +0300

    refactor: rebuild main to only get config and start application

--- cmd/ShopAPI/main.go
+++ cmd/ShopAPI/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
+	"github.com/sonofsun61/APIFromSpec/internal/app"
 	"github.com/sonofsun61/APIFromSpec/internal/config"
-	"github.com/sonofsun61/APIFromSpec/internal/database"
 )
 
 func main() {
 	cfg := config.MustLoadConfig()
-	ctx := context.Background()
-	pool := database.MustConnectToDatabase(ctx, cfg.ConnString)
-	defer pool.Close()
-	fmt.Println("connected to database successfully")
+	application := app.NewApp(cfg)
+	if err := application.Run(); err != nil {
+		panic(err)
+	}
 }
