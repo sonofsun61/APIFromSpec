4fa250de33e4d55348fc971338e7797d9ecefd5b
tarnovgm <glebtarn@gmail.com>
2026-09-25 10:34:43 +0300

    feat (router): add new supplier endpoints

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -6,7 +6,7 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func SetUpRouter(clientHandler *ClientHandler) http.Handler {
+func SetUpRouter(clientHandler *ClientHandler, supplierHandler *SupplierHandler) http.Handler {
 	r := chi.NewRouter()
 	r.Route("/api/v1", func(r chi.Router)  {
 		r.Route("/clients", func(r chi.Router) {
@@ -14,6 +14,13 @@ func SetUpRouter(clientHandler *ClientHandler) http.Handler {
 			r.Delete("/{id}", clientHandler.DeleteClientByID)
 			r.Get("/", clientHandler.GetClients)
 			r.Patch("/{id}", clientHandler.UpdateClientAddress)
+		})
+		r.Route("/suppliers", func(r chi.Router) {
+			r.Post("/", supplierHandler.AddSupplier)
+			r.Patch("/{id}", supplierHandler.UpdateSupplierAddress)
+			r.Delete("/{id}", supplierHandler.DeleteSupplier)
+			r.Get("/", supplierHandler.GetSuppliers)
+			r.Get("/{id}", supplierHandler.GetSupplierByID)
 		})
 	})
 	return r
